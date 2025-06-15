a36a3721402559118f0a19fa331b842c3a134178
Richard Wasonga <rowwasonga@gmail.com>
2025-06-15 10:56:47 +0300

    added reastaurantpizza model code

---
+++ server/models/restaurant_pizza.py
@@ -0,0 +1,18 @@
+from . import db,SerializerMixin
+from sqlalchemy.orm import validates
+
+class RestaurantPizza(db.Model, SerializerMixin):
+    __tablename__= 'restaurant_pizzas'
+
+    id = db.Column(db.Integer, primary_key=True)
+    price db.Column(db.Integer, nullable=False)
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'), nullable=False)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'), nullable=False)
+    pizza = db.relationship('Pizza', back_populates='pizza_restaurants')
+    restaurant = db.relationship('Restaurant', back_populates='restaurant_pizzas')
+
+    @validates('price')
+    def validate_price(self, key, value):
+        if value < 1 or value > 30:
+            raise ValueError("Price must be between 1 and 30")
+        return value

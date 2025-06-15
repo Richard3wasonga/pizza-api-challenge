5e912713de3520ded35ac3393e1f9b9439c7358c
Richard Wasonga <rowwasonga@gmail.com>
2025-06-15 10:32:05 +0300

    added restaurant model code

---
+++ server/models/restaurant.py
@@ -0,0 +1,11 @@
+from . import db, SerializerMixin
+from sqlalchemy.ext.associationproxy import association_proxy
+
+class Restaurant(db.Model, SerializerMixin):
+    __tablename__='restaurants'
+
+    id = db.Columns(db.Integer, primary_key=True)
+    name = db.Column(db.string, nullable=False)
+    address =db.Column(db.String, nullable=False)
+    restaurant_pizzas = db.relationship('RestaurantPizza', back_populates='restaurant', cascade='all, delete')
+    pizzas = association_proxy('restaurant_pizzas', 'pizza')
